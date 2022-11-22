2eb50293d06d2f74ff3207a64b66d834b5e9515b
AlexaWhitDU <112434021+AlexaWhitDU@users.noreply.github.com>
2022-11-22 13:17:21 -0700

    completed current weather data need to fix bug in compare two cities

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from pprint import PrettyPrinter
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, send_file
-from geopy.geocoders import Nominatim
+# from geopy.geocoders import Nominatim
 
 
 ################################################################################
@@ -45,20 +45,27 @@ def results():
     """Displays results for current weather conditions."""
     # TODO: Use 'request.args' to retrieve the city & units from the query
     # parameters.
-    city = ''
-    units = ''
+    
+    appid = os.getenv('API_KEY')
+    city = request.args.get('city')
+    units = request.args.get('units')
+    API_URL = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}&units={units}'
+    print(API_URL)
 
-    params = {
-        # TODO: Enter query parameters here for the 'appid' (your api key),
-        # the city, and the units (metric or imperial).
-        # See the documentation here: https://openweathermap.org/current
+    # params = {
+    #      TODO: Enter query parameters here for the 'appid' (your api key),
+    #     # the city, and the units (metric or imperial).
+    #     # See the documentation here: https://openweathermap.org/current
+    #     'appid': appid,
+    #     'city': city,
+    #     'units': units,
+    # }
 
-    }
-
-    result_json = requests.get(API_URL, params=params).json()
+    result_json = requests.get(API_URL).json()
 
     # Uncomment the line below to see the results of the API call!
-    # pp.pprint(result_json)
+    pp.pprint(result_json)
+
 
     # TODO: Replace the empty variables below with their appropriate values.
     # You'll need to retrieve these from the result_json object above.
@@ -66,16 +73,20 @@ def results():
     # For the sunrise & sunset variables, I would recommend to turn them into
     # datetime objects. You can do so using the `datetime.fromtimestamp()` 
     # function.
+
+    stringDate = (datetime.now()).strftime('%A, %B, %d, %Y')
+   
+
     context = {
-        'date': datetime.now(),
-        'city': '',
-        'description': '',
-        'temp': '',
-        'humidity': '',
-        'wind_speed': '',
-        'sunrise': '',
-        'sunset': '',
-        'units_letter': get_letter_for_units(units)
+        'date': stringDate,
+        'city': result_json['name'],
+        'description': result_json['weather'][0]['description'],
+        'temp': result_json['main']['temp'],
+        'humidity': result_json['main']['humidity'],
+        'wind_speed': result_json['wind']['speed'],
+        'sunrise': datetime.fromtimestamp(result_json['sys']['sunrise']),
+        'sunset': datetime.fromtimestamp(result_json['sys']['sunset']),
+        'units_letter': get_letter_for_units(units),
     }
 
     return render_template('results.html', **context)
@@ -86,24 +97,56 @@ def comparison_results():
     """Displays the relative weather for 2 different cities."""
     # TODO: Use 'request.args' to retrieve the cities & units from the query
     # parameters.
-    city1 = ''
-    city2 = ''
-    units = ''
+    appid = os.getenv('API_KEY')
+    city1 = request.args.get('city1')
+    city2 = request.args.get('city2')
+    units = request.args.get('units')
+    city1_data = compare_helper(city1, units)
+    city2_data = compare_helper(city2, units)
 
     # TODO: Make 2 API calls, one for each city. HINT: You may want to write a 
     # helper function for this!
 
 
+    stringDate = (datetime.now()).strftime('%A, %B, %d, %Y')
+
     # TODO: Pass the information for both cities in the context. Make sure to
     # pass info for the temperature, humidity, wind speed, and sunset time!
     # HINT: It may be useful to create 2 new dictionaries, `city1_info` and 
     # `city2_info`, to organize the data.
-    context = {
 
+    first_city = {
+        'date': stringDate,
+        'city': city1_data['name'],
+        'temp': city1_data['main']['temp'],
+        'humidity': city1_data['main']['humidity'],
+        'wind_speed': city1_data['wind']['speed'],
+        'sunset': datetime.fromtimestamp(city1_data['sys']['sunset']),
+        'units_letter': get_letter_for_units(units),
+    }
+
+    second_city = {
+        'date': stringDate,
+        'city': city2_data['name'],
+        'temp': city2_data['main']['temp'],
+        'humidity': city2_data['main']['humidity'],
+        'wind_speed': city2_data['wind']['speed'],
+        'sunset': datetime.fromtimestamp(city2_data['sys']['sunset']),
+        'units_letter': get_letter_for_units(units),
+    }
+    
+    context = {
+        'first_city': first_city,
+        'second_city': second_city
     }
 
     return render_template('comparison_results.html', **context)
 
+def compare_helper(city, units):
+    appid = os.getenv('API_KEY')
+    API_URL = (f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}&units={units}')
+    response = requests.get(API_URL).json()
+    return response
 
 if __name__ == '__main__':
     app.config['ENV'] = 'development'
